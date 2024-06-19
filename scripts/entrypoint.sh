57af4a6f9374dc271bbb7f3fcd16849a6ecedaee
Tyler Greer <tgreer812@gmail.com>
2024-06-19 01:14:29 -0400

    f
    f

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,15 @@ echo "Running entrypoint script..."
 echo "Current directory: $(pwd)"
 echo "Contents of current directory: $(ls -la)"
 
+
+# Running entrypoint script...
+# Current directory: /app
+# Contents of current directory: total 12
+# drwxr-xr-x 1 root root 4096 Jun 19 05:06 .
+# drwxr-xr-x 1 root root 4096 Jun 19 05:12 ..
+# lrwxrwxrwx 1 root root   11 Jun 19 05:06 server -> /mnt/server
+# -rwxr-xr-x 1 root root 3497 Jun 19 05:05 start-minecraft.sh
+
+
 # Run the start-minecraft.sh script
-# /app/start-minecraft.sh
-/bin/sh
+cd ./server && /app/start-minecraft.sh
