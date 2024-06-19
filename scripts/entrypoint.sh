c6f00bee0080ca800004c2ef45923bd0a1a5dd4a
Tyler Greer <tgreer812@gmail.com>
2024-06-19 00:37:52 -0400

    idek now

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,9 +5,5 @@ echo "Running entrypoint script..."
 echo "Current directory: $(pwd)"
 echo "Contents of current directory: $(ls -la)"
 
-# Switch to the server directory
-# and start the Minecraft server
-cd ./server || exit 1
-
 # Run the start-minecraft.sh script
-/app/start-minecraft.sh
+cd /app/server && /app/start-minecraft.sh
