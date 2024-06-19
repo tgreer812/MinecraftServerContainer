e9e11de2a4fad28bef04b0d8b7141e1912f819dc
Tyler Greer <tgreer812@gmail.com>
2024-06-19 01:30:56 -0400

    Now mounts a fileshare and loads correctly
    Verified to persistently store server files on the /mnt/server folder if it's mounted as a volume

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Ensure the server path is set
+# TODO: Test if this actually works if the user sets the MOUNT_PATH environment variable
+# might not work because of the way docker does layers
 : ${MOUNT_PATH:=/mnt/server}
 
 # Export the environment variable
