d23ec741c1411c7a317f8b2a56be7ed940252a7c
Tyler Greer <tgreer812@gmail.com>
2024-06-23 19:35:53 -0400

    Fixed environment variable name bug

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,18 +5,11 @@ echo "Running entrypoint script..."
 echo "Current directory: $(pwd)"
 echo "Contents of current directory: $(ls -la)"
 
-
-# Running entrypoint script...
-# Current directory: /app
-# Contents of current directory: total 12
-# drwxr-xr-x 1 root root 4096 Jun 19 05:06 .
-# drwxr-xr-x 1 root root 4096 Jun 19 05:12 ..
-# lrwxrwxrwx 1 root root   11 Jun 19 05:06 server -> /mnt/server
-# -rwxr-xr-x 1 root root 3497 Jun 19 05:05 start-minecraft.sh
-
+# List all the environment variables for debugging purposes
+env
 
 # Run the start-minecraft.sh script
-if [ "$USE_FORGE" = "true" ]; then
+if [ "$USE_FORGE_ENV" = "true" ]; then
   echo "Starting Minecraft Forge server..."
   cd ./server && /app/start-forge-server.sh
 else
